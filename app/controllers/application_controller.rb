401fde35f6435ac96f39add0758113835c586766
Will Studabaker <will.studabaker@gmail.com>
2020-12-17 11:48:45 -0500

    commit tree 41095cc5089e9494d01fb5739cdf869c0a8205ce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,37 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  post '/articles' do
+    @article = Article.find_or_create_by(title: params[:title], content: params[:content])
+    redirect "/articles/#{Article.last.id}"
+  end
+  get '/articles/new' do
+    erb :new
+  end
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    erb :show
+  end
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+  #  raise params.inspect
+    erb :edit
+  end
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    @articles = Article.all
+    erb :index
+  end
 end
